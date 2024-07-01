6ced189d6dbe05bb84a3199f6354924863f9044d
Suhas <51376855+suhasbn@users.noreply.github.com>
2024-06-30 22:06:57 -0700

    Create final_serve_media.py

---
+++ final_serve_media.py
@@ -0,0 +1,24 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+    def do_OPTIONS(self):
+        self.send_response(200, "ok")
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
+
+def run(port=8070):
+    os.chdir('/tmp')  # Change to the directory where temporary files are stored
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, CORSRequestHandler)
+    print(f"Serving files on port {port}")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
